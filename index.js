18cdcafc9cc507218d47e99ae2cd32f48c35a20a
serapath <dev@serapath.de>
2016-09-03 10:18:05 +0200

    UPDATE use cases

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ module.exports = djoin
 function djoin (/*arr*/path, /*str*/delimiter) {
   if (!xArray(path)) return ''
   if (!xString(delimiter) || !delimiter.length) delimiter = '/'
-  if (path[path.length-1] === '') path.push('')
   return path.join(delimiter)
 }
